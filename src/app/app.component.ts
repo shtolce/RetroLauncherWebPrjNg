74ffce17cf68978a1c09ade3f5dccc0247b61696
Dmitry Kim <kimdimka@yandex.ru>
2019-08-14 23:13:38 +1000

    Закончил фильтры и поиск компоненты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,16 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   itemsPerPage = 3;
   games = [];
+  favoritesArray = []
   currentPage: number = 1;
-
+  mappedGenresArr;
+  mappedPlatformesArr;
+  searchText = '';
   ngOnInit() {
+    this.favoritesArray.push({
+      shortName:     'Vasya'
+    });
+
     this.games.push({
       shortName:     'Vasya',
       fullName:      'Poniy Vasya',
@@ -32,19 +39,92 @@ export class AppComponent implements OnInit {
       genreName:     'Arcade'
     })
     this.games.push({
+      shortName:     'MarioII',
+      fullName:      'Super Mario Bros.II',
+      platformType:  'PS4',
+      year:          1991,
+      genreName:     'Arcade'
+    })
+    this.games.push({
       shortName:     'Igor Zaebal',
       fullName:      'Igor Zaebal Legacy.',
       platformType:  'Dendy',
       year:          2018,
       genreName:     'Advanture'
     })
+    this.getPlatformes();
+    this.getGenres();
   };
   changePage(numP: number) {
     this.currentPage = numP;
   }
   get gamesArr():any {
     let pageIndex = (this.currentPage-1) * this.itemsPerPage
-    return this.games.slice(pageIndex,pageIndex + this.itemsPerPage);
+    let gameFilterArr;
+    gameFilterArr = this.games.filter((data)=>{
+      let tempGenreArr = this.mappedGenresArr.filter((el)=>el.checked).map((item=>item.genre));
+      return tempGenreArr.includes(data.genreName);
+    });
+    gameFilterArr = gameFilterArr.filter((data)=>{
+      let tempPlatformesArr = this.mappedPlatformesArr.filter((el)=>el.checked).map((item=>item.platform));
+      return tempPlatformesArr.includes(data.platformType);
+    });
+
+    gameFilterArr = gameFilterArr.filter((data)=>{
+      return data.shortName.indexOf(this.searchText) != -1;
+
+
+    });
+
+    return gameFilterArr.slice(pageIndex,pageIndex + this.itemsPerPage);
+
   }
+  getGenres() {
+    this.mappedGenresArr = this.games.map((data)=>{
+      return {
+        'genre':data.genreName,
+        'checked':true
+      }
+    });
+    this.mappedGenresArr = this.mappedGenresArr.filter((item,pos)=>{
+      return this.mappedGenresArr.map((data)=>data.genre).indexOf(item.genre) == pos
+    });
+  }
+
+  getPlatformes() {
+    this.mappedPlatformesArr = this.games.map((data)=>{
+      return {
+        'platform':data.platformType,
+        'checked':true
+      }
+    });
+    this.mappedPlatformesArr =this.mappedPlatformesArr.filter((item,pos)=>{
+      return this.mappedPlatformesArr.map((data)=>data.platform).indexOf(item.platform) == pos
+    });
+  }
+
+  get genres() {
+    return this.mappedGenresArr;
+  }
+
+  get platformes() {
+    return this.mappedPlatformesArr;
+  }
+
+  onGenreCheck(target) {
+    this.mappedGenresArr= target;
+
+  }
+  onPlatformCheck(target) {
+    this.mappedPlatformesArr= target;
+
+
+  }
+
+  search(text) {
+    this.searchText = text;
+  }
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    FilterComponent,
+    HeaderComponent,
+    FavoritesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
